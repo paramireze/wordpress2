01cd4bf4eae550aafc9c34c62835bf9796ef9956
paramireze <paramireze@gmail.com>
2017-04-04 11:18:10 -0500

    Finished video tutorial 1
    https://www.youtube.com/watch?v=tSblOUw97Mc&list=PLIjMj0-5C8TI7Jwell1rTv
    v5XXyrbKDcy

--- wp-content/plugins/basic-plugin/basic-plugin.php
+++ wp-content/plugins/basic-plugin/basic-plugin.php
@@ -1,5 +1,15 @@
 <?php
 /**
  * Plugin Name: Basic Plugin
- * Plugin URI: http://
+ * Plugin URI: https://github.com/paramireze/wordpress2
+ * Description: This plugin with be displaying Job opportunities
+ * Author: Paul Ramirez
+ * Version: 0.0.1
  */
+
+function dwwp_do_something_cool() {
+    // do something
+}
+
+add_action('a_hook','dwwp_do_something_cool');
+add_filter('a_hook','dwwp_do_something_cool');
