126fa497856ef45b5a37112e7757eef0c7369dd5
paramireze <paramireze@gmail.com>
2017-04-04 15:28:00 -0500

    Third tutorial
    https://www.youtube.com/watch?v=S_r0KR0D2PY&t=906s

--- wp-content/plugins/basic-plugin/basic-plugin.php
+++ wp-content/plugins/basic-plugin/basic-plugin.php
@@ -7,9 +7,23 @@
  * Version: 0.0.1
  */
 
-function dwwp_do_something_cool() {
+function dwwp_remove_dashboard_widget() {
     // do something
+    remove_meta_box('dashboard_primary', 'dashboard', 'post_container_1');
 }
 
-add_action('a_hook','dwwp_do_something_cool');
-add_filter('a_hook','dwwp_do_something_cool');
+add_action('wp_dashboard_setup','dwwp_remove_dashboard_widget');
+
+function dwwp_add_google_link() {
+
+    global $wp_admin_bar;
+
+    $wp_admin_bar->add_menu( array(
+        'id'    => 'google_analytics',
+        'title' => 'Google Analytics',
+        'href'  => 'http://google.com/analytics'
+    ) );
+}
+
+add_action('wp_before_admin_bar_render', 'dwwp_add_google_link');
+
