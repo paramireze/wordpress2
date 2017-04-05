78d0da9ecbbec1ec24af036947093af86a1d6a86
paramireze <paramireze@gmail.com>
2017-04-05 15:49:42 -0500

    Wordpress tutorial 7 - taxonomies
    https://www.youtube.com/watch?v=agooLNf0pEQ

--- wp-content/plugins/wp-job-listing/wp-job-listing.php
+++ wp-content/plugins/wp-job-listing/wp-job-listing.php
@@ -71,4 +71,43 @@ function dwwp_register_post_type()
 }
 
 add_action('init', 'dwwp_register_post_type');
+
+function dwwp_register_taxonomy() {
+
+    $plural = 'Locations';
+    $singular = 'Location';
+
+    $labels = array(
+        'name'              => $plural,
+        'singular_name'     => $singular,
+        'search_item'       => 'Search ' . $plural,
+        'popular_item'      => 'Popular ' . $plural,
+        'all_items'         => 'All ' . $plural,
+        'parent_item'       => null,
+        'parent_item_colon' => null,
+        'edit_item'         => 'Edit ' . $singular,
+        'update_item'       => 'Update ' . $singular,
+        'add_new_item'      => 'Add New ' . $singular,
+        'new_item_name'     => 'New ' . $singular . ' Name',
+        'separate_items_with_commas' => 'Separate  ' . $plural . 'with commas',
+        'add_or_remove_items' => 'Add or remove ' . $plural,
+        'choose_from_most_used' => 'Choose from the most used ' . $plural,
+        'not_found'         => 'No ' . $plural . ' found',
+        'menu_name'         => $plural
+    );
+
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'         => true,
+        'rewrite'           => array( 'arr')
+    );
+
+    register_taxonomy( 'location', 'job', $args );
+}
+
+add_action('init', 'dwwp_register_taxonomy');
 ?>
