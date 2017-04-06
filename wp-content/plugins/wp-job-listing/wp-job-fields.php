b92301bf2ee9020259e3144925dfaf440c434709
paramireze <paramireze@gmail.com>
2017-04-06 10:30:55 -0500

    Tutorial 10 custom boxes in meta fields
    Went over creating a text area box with controls. Also added media
    button
    
    https://www.youtube.com/watch?v=y3SvtpOk4UI&t=620s

--- wp-content/plugins/wp-job-listing/wp-job-fields.php
+++ wp-content/plugins/wp-job-listing/wp-job-fields.php
@@ -11,3 +11,36 @@
     }
 
     add_action('add_meta_boxes', 'dwwp_add_custom_metabox');
+
+    function dwwp_meta_callback() {
+        ?>
+        <div>
+            <div class="meta-row">
+                <div class="meta-th">
+                    <label for="job-id" class="dwwp-row-title">Job ID</label>
+                </div>
+                <div class="meta-td">
+                    <input type="text" name="job-id" id="job-id" value="">
+                </div>
+            </div>
+        </div>
+        <div class="meta">
+            <div class="meta-th">
+                <span>Principle Duties</span>
+            </div>
+        </div>
+        <div class="meta-editor">
+            <?php
+            $content = get_post_meta($post->ID, 'principle_duties', true);
+            $editor = 'principle_duties';
+            $settings = array(
+                'textarea_rows' => 8,
+                'media_buttons' => true
+            );
+            wp_editor($content, $editor, $settings);
+
+            ?>
+        </div>
+        <?php
+
+    }
