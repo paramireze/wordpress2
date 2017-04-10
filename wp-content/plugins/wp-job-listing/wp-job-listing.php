12740d58de17f5aeb091a9b182d727c2c336fed8
paramireze <paramireze@gmail.com>
2017-04-09 20:15:07 -0500

    Tutorial 11 - learn to hook in scripts using wpa_enqueue_script
    https://www.youtube.com/watch?v=yMuUfx-PnTc

--- wp-content/plugins/wp-job-listing/wp-job-listing.php
+++ wp-content/plugins/wp-job-listing/wp-job-listing.php
@@ -17,3 +17,15 @@ if (!defined('ABSPATH')) {
 require ( plugin_dir_path(__FILE__) .'wp-job-cpt.php' );
 require ( plugin_dir_path(__FILE__) .'wp-job-render-admin.php' );
 require ( plugin_dir_path(__FILE__) .'wp-job-fields.php' );
+
+function dwwp_admin_enqueue_scripts() {
+    global $pagenow, $typenow;
+    if (($pagenow == 'post.php' || $pagenow == 'post-new.php') && $typenow == 'job') {
+        wp_enqueue_style('dwwp-admin-css', plugins_url('css/admin-jobs.css', __FILE__));
+        wp_enqueue_script('dwwp-job-js', plugins_url('js/admin-jobs.js', __FILE__), array('jquery', 'jquery-ui-datepicker'), '20150204', false );
+        wp_enqueue_style('jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
+        wp_enqueue_script('dwwp-custom-quicktags', plugins_url('js/dwwp-quicktags.js', __FILE__ ), array('quicktags'), '20170206', true);
+    }
+}
+add_action('admin_enqueue_scripts', 'dwwp_admin_enqueue_scripts', 'jquery-style', 'dwwp-custom-quicktags');
+
