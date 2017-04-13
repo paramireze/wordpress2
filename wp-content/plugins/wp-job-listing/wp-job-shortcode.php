2cf6bbb6700f775c12619ebd94cb080eccdf18d1
paramireze <paramireze@gmail.com>
2017-04-13 18:26:41 -0500

    Tutorial 21 - more shortcake
    https://www.youtube.com/watch?v=VTE5U_QhyNU&t=905s

--- wp-content/plugins/wp-job-listing/wp-job-shortcode.php
+++ wp-content/plugins/wp-job-listing/wp-job-shortcode.php
@@ -1,17 +1,34 @@
 <?php
-function dwwp_sample_shortcode( $atts, $content = null ) {
+function dwwp_job_taxonomy_list( $atts, $content = null ) {
 
     $atts = shortcode_atts(
         array(
-            'title' => 'default title',
-            'src' => 'www.google.com'
+            'title' => 'Current job openings in...'
         ), $atts
     );
 
-//    print_r($atts);
-//    print_r($content);
-    return '<h1>' . $atts['title'] . '</h1>';
+    $locations = get_terms( 'location' );
+
+    if ( !empty( $locations ) && ! is_wp_error( $locations ) ) {
+
+        $displayList  = '<div id="job-location-list">';
+        $displayList .= '<h4>' . esc_html__( $atts['title'] ) . '</h4>';
+        $displayList .= '<ul>';
+        foreach( $locations as $location) {
+
+            $displayList .= '<li class="job-location">';
+            $displayList .= '<a href="' . esc_url(get_term_link( $location )) . '">';
+            $displayList .= esc_html__( $location->name );
+            $displayList .= '</a>';
+            $displayList .= '</li>';
+
+        }
+
+        $displayList .= '</ul></div>';
+
+    }
+
+    return $displayList;
 }
 
-
-add_shortcode( 'job_listing', 'dwwp_sample_shortcode' );
+add_shortcode( 'job_location_list', "dwwp_job_taxonomy_list" );
