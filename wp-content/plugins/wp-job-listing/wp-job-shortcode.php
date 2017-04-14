eb9f38a0a737990cca7b9de670f85ed407880a53
paramireze <paramireze@gmail.com>
2017-04-14 16:20:04 -0500

    Tutorial 23 - looping over posts and displaying them in formatted html
    http://localhost:8888/wordpress2/wp-admin/post.php?post=115&action=edit

--- wp-content/plugins/wp-job-listing/wp-job-shortcode.php
+++ wp-content/plugins/wp-job-listing/wp-job-shortcode.php
@@ -34,14 +34,18 @@ function dwwp_job_taxonomy_list( $atts, $content = null ) {
 add_shortcode( 'job_location_list', "dwwp_job_taxonomy_list" );
 
 function dwwp_list_job_by_location( $atts, $content = null ) {
-    $atts = shortcode_atts( array(
+
+    if (!isset($atts['location'])) {
+        return '<p class="job-error">You must enter a location</p>';
+    }
+    $atts = shortcode_atts(array(
         'title' => 'Current Job Openings in',
         'count' => 5,
         'location' => '',
-        'pagination' =>false
-    ), $atts );
+        'pagination' => false
+    ), $atts);
 
-    $paged = get_query_var('paged') ? get_query_var( 'paged' ) : 1;
+    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
 
     $args = array(
         'post_type' => 'job',
@@ -58,10 +62,37 @@ function dwwp_list_job_by_location( $atts, $content = null ) {
         )
     );
 
-    $jobs_by_location = new WP_Query( $args );
+    $jobs_by_location = new WP_Query($args);
 
-    echo '<pre>';
-    var_dump($jobs_by_location);
-}
+    if ($jobs_by_location->have_posts())  :
+        $location = str_replace('-', ' ', $atts['location']);
 
-add_shortcode('jobs_by_location', 'dwwp_list_job_by_location');
+        $display_by_location = '<div id="jobs-by-location">';
+        $display_by_location .= '<h4>' . esc_html__($atts["title"]) . '&nbsp' . esc_html__(ucwords($location)) . '</h4>';
+        $display_by_location .= '<ul>';
+
+        while ($jobs_by_location->have_posts()) : $jobs_by_location->the_post();
+            global $post;
+
+            $deadline = get_post_meta(get_the_ID(), 'application_deadline', true);
+            $title = get_the_title();
+            $slug = get_permalink();
+
+            $display_by_location .= '<li class="job-listing">';
+            $display_by_location .= sprintf('<a href="%s">%s</a>&nbsp&nbsp', esc_url($slug), esc_html__($title));
+            $display_by_location .= '<span>' . esc_html($deadline) . '</span>';
+            $display_by_location .= '</li>';
+
+        endwhile;
+
+        $display_by_location .= '</ul>';
+        $display_by_location .= '</div>';
+
+        wp_reset_postdata();
+
+        endif;
+
+        return $display_by_location;
+    }
+
+    add_shortcode('jobs_by_location', 'dwwp_list_job_by_location');
